e3eb8bc6aa71de35067fc3193f99028836efa736
KhrystynaLutsiv <132158280+KhrystynaLutsiv@users.noreply.github.com>
2024-03-04 23:13:51 +0200

    Add files via upload

--- Christina_Lutsiv/lab4/main.py
+++ Christina_Lutsiv/lab4/main.py
@@ -1,7 +1,6 @@
-def hello(name):
-    massage = "Hello, " + name + "!"
-    return massage
+def triplet(x, y=2, z=1):
+    return x * y * z
 
-user = "Ігор"
-output = hello(user)
-print(output)
+result1 = triplet(3)       # x=3, y=2 (за замовчуванням), z=1 (за замовчуванням)
+result2 = triplet(3, 4)    # x=3, y=4, z=1 (за замовчуванням)
+result3 = triplet(3, 4, 5) # x=3, y=4, z=5
