dd908c936e1cccba948ea8cddfda286c7084a3ab
coffee9ka <uzer13679@gmail.com>
2024-03-11 20:15:26 +0200

    add lab3

---
+++ Krisa_Bogdan/lab3/4.py
@@ -0,0 +1,51 @@
+#1
+#The following code example would print the data type of x, what data type would that be?
+x = 5
+print(type(x))
+
+#int
+
+#2
+#The following code example would print the data type of x, what data type would that be?
+x = "Hello World"
+print(type(x))
+
+#str
+
+#3
+#The following code example would print the data type of x, what data type would that be?
+x = 20.5
+print(type(x))
+
+#float
+
+#4
+#The following code example would print the data type of x, what data type would that be?
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+#list
+
+#5
+#The following code example would print the data type of x, what data type would that be?
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+#tuple
+
+#6
+#The following code example would print the data type of x, what data type would that be?
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+#dict
+
+#7
+#The following code example would print the data type of x, what data type would that be?
+
+x = True
+print(type(x))
+
+#bool
