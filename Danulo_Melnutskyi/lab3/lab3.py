4b25412faa315d7f3e02232af7286e5759490781
Danulo_0 <114855095+ilovepatience@users.noreply.github.com>
2024-03-25 23:54:12 +0200

    Create lab3.py

---
+++ Danulo_Melnutskyi/lab3/lab3.py
@@ -0,0 +1,29 @@
+# №1
+x = 5
+print(type(x))
+
+# №2
+x = "Hello World"
+print(type(x))
+
+# №3
+x = 20.5
+print(type(x))
+
+# №4
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+# №5
+x = ("apple", "banana", "cherry")
+print(type(x))
+
+# №6
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+# №7
+x = True
+print(type(x))
+
+
