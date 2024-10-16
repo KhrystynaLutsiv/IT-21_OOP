8d581050fc446bb445ae558a220fa7315c10a82b
TarasKuzemskyi <trex3741@gmail.com>
2024-10-16 18:31:28 +0300

    Add Lab11

--- Kuzemskyi_Taras/Lab11/Lab11.py
+++ Kuzemskyi_Taras/Lab11/Lab11.py
@@ -1,24 +1,19 @@
-# Базовий клас для всіх типів автомобілів
 class Car:
     def drive(self):
         raise NotImplementedError("Цей метод має бути перевизначений у дочірньому класі")
 
-# Легковий автомобіль (Sedan)
 class Sedan(Car):
     def drive(self):
         return "Керування легковим автомобілем"
 
-# Вантажівка (Truck)
 class Truck(Car):
     def drive(self):
         return "Керування вантажівкою"
 
-# Спортивний автомобіль (SportsCar)
 class SportsCar(Car):
     def drive(self):
         return "Керування спортивним автомобілем"
 
-# Фабрика для створення об'єктів автомобілів
 class CarFactory:
     @staticmethod
     def create_car(car_type):
@@ -31,23 +26,18 @@ class CarFactory:
         else:
             raise ValueError(f"Невідомий тип автомобіля: {car_type}")
 
-# Використання фабрики
 factory = CarFactory()
 
-# Створення легкового автомобіля
 sedan = factory.create_car('sedan')
-print(sedan.drive())  # Виведе: Керування легковим автомобілем
+print(sedan.drive())
 
-# Створення вантажівки
 truck = factory.create_car('truck')
-print(truck.drive())  # Виведе: Керування вантажівкою
+print(truck.drive())
 
-# Створення спортивного автомобіля
 sports_car = factory.create_car('sportscar')
-print(sports_car.drive())  # Виведе: Керування спортивним автомобілем
+print(sports_car.drive())
 
-# Спроба створити автомобіль невідомого типу
 try:
     unknown_car = factory.create_car('motorbike')
 except ValueError as e:
-    print(e)  # Виведе: Невідомий тип автомобіля: motorbike
+    print(e)
