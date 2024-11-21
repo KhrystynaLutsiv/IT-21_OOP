57a1a3225a64c2889d21571a802f25b5cf0f1805
Роман Тупісь <tupis.roman2004@gmail.com>
2024-11-21 11:30:34 +0200

    redacted code.py

--- Tupis_Roman/lab14/code.py
+++ Tupis_Roman/lab14/code.py
@@ -1,3 +1,21 @@
+#поганий приклад
+class OrderManager:
+    def create_order(self, data):
+        # Логіка створення замовлення
+        pass
+
+    def send_confirmation_email(self, order):
+        # Логіка відправлення електронного листа
+        pass
+
+    def log_order(self, order):
+        # Логіка журналювання замовлення
+        pass
+
+
+
+#приклад згідно SRP
+
 class OrderCreator:
     def create_order(self, data):
         # Логіка створення замовлення
@@ -12,3 +30,5 @@ class Logger:
     def log_order(self, order):
         # Логіка журналювання замовлення
         pass
+
+
