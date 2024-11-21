6b38a5c0b342ba15e70d7fc98ca35970697ec272
Роман Тупісь <tupis.roman2004@gmail.com>
2024-11-21 11:28:41 +0200

    add code.py

---
+++ Tupis_Roman/lab14/code.py
@@ -0,0 +1,14 @@
+class OrderCreator:
+    def create_order(self, data):
+        # Логіка створення замовлення
+        pass
+
+class EmailService:
+    def send_confirmation_email(self, order):
+        # Логіка відправлення електронного листа
+        pass
+
+class Logger:
+    def log_order(self, order):
+        # Логіка журналювання замовлення
+        pass
