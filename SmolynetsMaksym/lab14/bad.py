f23623484228da6b3c9f9120aa628cf0ca1f4fd4
Maksym <maks.smolinets1978@gmail.com>
2024-12-04 09:47:51 +0200

    change num lab14-15

--- SmolynetsMaksym/lab14/bad.py
+++ SmolynetsMaksym/lab14/bad.py
@@ -2,10 +2,8 @@ class PaymentProcessor:
     @staticmethod
     def process_payment(payment_type, amount):
         if payment_type == "credit_card":
-            # Обробка платежу через кредитну картку
             pass
         elif payment_type == "paypal":
-            # Обробка платежу через PayPal
             pass
         else:
             raise ValueError("Unknown payment type")
